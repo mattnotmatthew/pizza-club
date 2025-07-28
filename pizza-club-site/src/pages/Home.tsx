411f1d65dd8b4cc2b1ef66df23930db37987b0a5
MattNotMatthew <mattoriente@gmail.com>
2025-07-27 20:26:59 -0500

    svg

--- pizza-club-site/src/pages/Home.tsx
+++ pizza-club-site/src/pages/Home.tsx
@@ -17,7 +17,7 @@ const Home: React.FC = () => {
       // Mobile - contain to fit without cutting off, positioned higher
       return {
         size: 'contain',
-        position: 'center 42%'
+        position: 'center 38%'
       };
     } else if (width < 1024) {
       // Tablet
